aaf05aa8420abc754214d86f9c659a75679db920
MPenava <mpenava@royanis.com>
2024-06-06 18:51:16 +0200

    feat: modal for changing password

--- src/features/pages/Dashboard.tsx
+++ src/features/pages/Dashboard.tsx
@@ -1,15 +1,55 @@
-import { Authenticator } from "@aws-amplify/ui-react";
+import { useState } from "react";
+
+import { AccountSettings, Authenticator } from "@aws-amplify/ui-react";
 
 import { Button } from "primereact/button";
+import { Dialog } from "primereact/dialog";
 
 const Dashboard = () => {
+  const [isVisibleChangePassModal, setVisibleChangePassModal] =
+    useState<boolean>(false);
+
+  const handleSuccess = () => {
+    // eslint-disable-next-line no-alert
+    alert("Password is successfully deleted!");
+    setVisibleChangePassModal(false);
+  };
   return (
-    <div className="flex flex-column">
-      <div>Dashboard</div>
+    <div className="flex flex-column gap-2">
+      <div className="flex justify-content-around">Dashboard</div>
 
       <Authenticator>
-        {({ signOut }) => <Button onClick={signOut}>Sign Out</Button>}
+        {({ signOut }) => (
+          <Button
+            onClick={signOut}
+            pt={{
+              root: { className: "flex justify-content-around" },
+            }}
+          >
+            Sign Out
+          </Button>
+        )}
       </Authenticator>
+
+      <Button
+        severity="secondary"
+        pt={{
+          root: { className: "flex justify-content-around" },
+        }}
+        onClick={() => setVisibleChangePassModal(true)}
+      >
+        Change password
+      </Button>
+      <Dialog
+        visible={isVisibleChangePassModal}
+        onHide={() => setVisibleChangePassModal(false)}
+        pt={{
+          header: { className: "p-0 border-none" },
+          content: { className: "p-5" },
+        }}
+      >
+        <AccountSettings.ChangePassword onSuccess={handleSuccess} />
+      </Dialog>
     </div>
   );
 };
