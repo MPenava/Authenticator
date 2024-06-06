094ddc3acf648c849b6650385f9f70451474233a
MPenava <mpenava@royanis.com>
2024-06-06 17:26:26 +0200

    feat: installing amplify dependencies

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,30 +1,5 @@
-import mainitobaBackground from "@assets/scribe4all_background.png";
-import { Button } from "primereact/button";
-
 const MainLayout = () => {
-  return (
-    <div
-      className="flex flex-column h-screen w-screen bg-cover bg-no-repeat"
-      style={{
-        background: `url(${mainitobaBackground})`,
-      }}
-    >
-      <div
-        className="flex p-5 w-screen overflow-auto"
-        style={{ height: "inherit" }}
-      >
-        <div className="flex gap-5 h-full w-screen">
-          <div className="h-full w-15rem bg-gray-100 p-5  border-round-xl shadow-2">
-            Sidebar
-          </div>
-          <div className="h-full w-full bg-white p-5  border-round-xl shadow-2 overflow-auto">
-            <p>Content</p>
-            <Button>Button</Button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>Main</div>;
 };
 
 export { MainLayout };
