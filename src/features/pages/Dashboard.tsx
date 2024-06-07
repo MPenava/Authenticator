f2a6bfbc402f9c85c215bf2c1083b25ecef4d266
MPenava <mpenava@royanis.com>
2024-06-07 10:08:20 +0200

    feat: global signOut after changing password

--- src/features/pages/Dashboard.tsx
+++ src/features/pages/Dashboard.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 import { AccountSettings, Authenticator } from "@aws-amplify/ui-react";
 
+import { signOut as globalSignOut } from "aws-amplify/auth";
+
 import { Button } from "primereact/button";
 import { Dialog } from "primereact/dialog";
 
@@ -11,25 +13,51 @@ const Dashboard = () => {
 
   const handleSuccess = () => {
     // eslint-disable-next-line no-alert
-    alert("Password is successfully deleted!");
+    alert(
+      "Password is successfully changed. You will be logged out in a few moments..."
+    );
     setVisibleChangePassModal(false);
+    globalSignOut({ global: true });
   };
+
+  const handleGlobalSignOut = () => {
+    globalSignOut({ global: true });
+  };
+
+  const handleSendRequest = () => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1").then((response) =>
+      response.json()
+    );
+  };
+
   return (
     <div className="flex flex-column gap-2">
       <div className="flex justify-content-around">Dashboard</div>
 
       <Authenticator>
-        {({ signOut }) => (
-          <Button
-            onClick={signOut}
-            pt={{
-              root: { className: "flex justify-content-around" },
-            }}
-          >
-            Sign Out
-          </Button>
+        {({ signOut, user }) => (
+          <div>
+            <Button
+              onClick={signOut}
+              pt={{
+                root: { className: "flex justify-content-around w-full" },
+              }}
+            >
+              Sign Out
+            </Button>
+            <p>{user?.username}</p>
+          </div>
         )}
       </Authenticator>
+
+      <Button
+        onClick={handleGlobalSignOut}
+        pt={{
+          root: { className: "flex justify-content-around" },
+        }}
+      >
+        Global Sign Out
+      </Button>
 
       <Button
         severity="secondary"
@@ -39,6 +67,15 @@ const Dashboard = () => {
         onClick={() => setVisibleChangePassModal(true)}
       >
         Change password
+      </Button>
+      <Button
+        severity="success"
+        pt={{
+          root: { className: "flex justify-content-around" },
+        }}
+        onClick={handleSendRequest}
+      >
+        Send request
       </Button>
       <Dialog
         visible={isVisibleChangePassModal}
